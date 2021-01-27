6b559ed01be918137690700ee32bd0e27a8022b6
ruhul <ruhul.cse7862@gmail.com>
2021-01-28 00:03:49 +0600

    movie add using context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
 import Nav from './components/Nav';
+import AddMovies from './components/AddMovies';
 import {MovieProdiver} from "./MovieContext";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <MovieProdiver>
       <div className="App">
         <Nav/>
+        <AddMovies/>
         <MovieList/> 
       </div>
     </MovieProdiver>
